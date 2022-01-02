e311fc58cd83f2f7ecb54f23573dc417993d5dec
Micaella Colombo <micaelc@g.clemson.edu>
2022-01-02 16:52:12 -0500

    commit tree f3afaaf4f923361e2ce07647632b88cbce11123b

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  get "students/:id", to: "students#show"
+  #get "students/:id", to: "students#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
+  #custom routes because it is not one of the 7 RESTful routes, has to be before the typical resources routes
+
+
   resources :students, only: [:index,:show]
 end
